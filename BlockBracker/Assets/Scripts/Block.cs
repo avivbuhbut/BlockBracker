9acade033b2109dbf35984fa9beae431023d5e11
avivbuhbut <avivb931@gmail.com>
2020-02-02 08:11:34 +0200

    finished level 1!

--- BlockBracker/Assets/Scripts/Block.cs
+++ BlockBracker/Assets/Scripts/Block.cs
@@ -6,7 +6,8 @@ public class Block : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject);
+        Destroy(gameObject); // destroying the game object
+        Debug.Log(collision.gameObject.name); // prints the name of the object that has been colided with the gameobject
     }
 
 }
